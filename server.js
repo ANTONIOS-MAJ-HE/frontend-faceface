adc6ea7a9ba7435370c549e0c09f24b2b3448180
ANTONIOS-MAJ-HE <95696217+ANTONIOS-MAJ-HE@users.noreply.github.com>
2023-12-19 09:01:24 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 import express from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
+
+// Define el puerto en el que se ejecutará la aplicación
+// Usa el puerto asignado por el entorno de producción o 3000 para desarrollo local
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'dist')));
@@ -11,5 +18,5 @@ app.get('*', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
