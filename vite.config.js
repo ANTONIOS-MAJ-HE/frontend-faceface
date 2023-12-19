527b5f044f221eab6eb818de0e4cfc1d48c658f9
ANTONIOS-MAJ-HE <95696217+ANTONIOS-MAJ-HE@users.noreply.github.com>
2023-12-19 03:38:24 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 const express = require('express');
 const path = require('path');
 const app = express();
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-
-  server: {
-    https: false,
-  },
-})
-
 
 const PORT = process.env.PORT || 3000;
 
